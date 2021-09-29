baeb96aaf6911448649b40d3f63ad4c24b625c5e
ycheng <36308352+ycheng22@users.noreply.github.com>
2021-09-28 22:44:50 -0400

    Add files via upload

---
+++ Full-Stack Web Dev with React/Projects/NodeJS/conFusionServer/models/promotions.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const promoSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    }
+},{
+    timestamps: true
+});
+
+var Promotions = mongoose.model('Promotion', promoSchema);
+module.exports = Promotions;
