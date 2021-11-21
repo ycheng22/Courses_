fd86f830b980c1173d1f7f44f97126222639dcbe
ycheng <36308352+ycheng22@users.noreply.github.com>
2021-11-20 21:35:02 -0600

    Add files via upload

---
+++ Java Programming and Software Engineering Fundamentals Specialization/c1w3/scripts.js
@@ -0,0 +1,13 @@
+function changecolor() {
+    var dd1 = document.getElementById("d1");
+    var dd2 = document.getElementById("d2");
+    dd1.className = "blueback";
+    dd2.className = "yellowback";
+}
+  
+  function changetext() {
+    var dd1 = document.getElementById("d1");
+    var dd2 = document.getElementById("d2");
+    dd1.innerHTML = "Bonjour";
+    dd2.innerHTML = "Sayonara";
+}
